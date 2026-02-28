1687e04cd574245d1fcf6421822c7ee49337595e
akhilkannur <akhil.1000what@gmail.com>
2026-02-28 22:44:31 +0530

    fix: import Link in ugc-client.tsx to resolve build error

--- app/ugc/ugc-client.tsx
+++ app/ugc/ugc-client.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react"
 import Image from "next/image"
+import Link from "next/link"
 import { Header } from "@/components/header"
 import { Marquee } from "@/components/marquee"
 import { StreetInterviewGraphic } from "@/components/street-interview-graphic"
